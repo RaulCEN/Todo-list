c6e0276571c0b8c05f411409a7757e275f7c1003
Raul Canul <raul.canul@choicehotels.com>
2024-05-29 10:26:14 -0600

    @feat: AddTodo component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 
+import AddTodo from './components/AddTodo/AddTodo';
+
 function App() {
   return (
     <div className="App">
       <header>
         <h1>Todo List</h1>
       </header>
+
+      <section>
+        <AddTodo loading={false} onAddTodo={(newtodo) => console.log(newtodo)} />
+      </section>
     </div>
   );
 }
---
+++ src/components/AddTodo/AddTodo.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+interface AddTodoProps {
+  loading: boolean;
+  onAddTodo: (newTodo: string) => void
+}
+
+const AddTodo: React.FunctionComponent<AddTodoProps> = ({ loading, onAddTodo }: AddTodoProps) => {
+  const [newTodoValue, setNewTodoValue] = useState<string>('')
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => setNewTodoValue(event.target.value)
+
+  const handleSubmitNewTodo = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (newTodoValue === '') return;
+
+    onAddTodo(newTodoValue);
+    setNewTodoValue('');
+  }
+
+  return (
+    <form onSubmit={handleSubmitNewTodo}>
+      <input
+        id='new_todo'
+        name='new_todo'
+        placeholder='New Todo'
+        disabled={loading}
+        value={newTodoValue || ''}
+        onChange={handleInputChange}
+      />
+
+      <button
+        type='submit'
+        disabled={loading || newTodoValue === ''}
+      >
+        Add
+      </button>
+    </form>
+  )
+}
+
+export default AddTodo
